f53b75a2a41079f8dbbeb40acf133bd0452c4a8d
MengHX <mhxdev@gmail.com>
2014-08-20 21:08:14 +0800

    update urls configuration

--- mxsite/urls.py
+++ mxsite/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from mxblog.views import user, blog, site
+from mxblog.views import user, post, site
 
 admin.autodiscover()
 
@@ -10,9 +10,9 @@ urlpatterns = patterns(
     url(r'^$', site.index, name='home'),
     url(r'^register/', user.register, name='register'),
     url(r'^login/', user.login, name='login'),
-    url(r'^post/(?P<bid>\d+)/', blog.blog, name='post'),
-    url(r'^thumbs/(?P<ctype>(support)|(oppose))/(?P<bid>\d+)/', blog.thumbs),
-    url(r'^archive/', blog.archive, name='archive'),
+    url(r'^post/(?P<bid>\d+)/', post.blog, name='post'),
+    url(r'^thumbs/(?P<ctype>(support)|(oppose))/(?P<bid>\d+)/', post.thumbs),
+    url(r'^archive/', post.archive, name='archive'),
     url(r'^start/', site.start, name='start'),
     url(r'^about/', site.about, name='about'),
     url(r'^ckeditor/', include('ckeditor.urls')),
